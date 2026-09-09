8189c5b953ac525e1aaab61c694853063b25a326
TheSkeletonSword <agustin.pereira.ro@gmail.com>
2026-09-08 22:18:07 -0300

    Delete kernel_module/.sysentinel_metrics.o.cmd

--- kernel_module/.sysentinel_metrics.o.cmd
+++
@@ -1,3 +0,0 @@
-savedcmd_sysentinel_metrics.o := ld -m elf_x86_64 -z noexecstack --no-warn-rwx-segments   -r -o sysentinel_metrics.o @sysentinel_metrics.mod  ; /usr/src/kernels/7.1.8-200.fc44.x86_64/tools/objtool/objtool --hacks=jump_label --hacks=noinstr --hacks=skylake --ibt --orc --retpoline --rethunk --sls --static-call --uaccess --prefix=16  --link  --module sysentinel_metrics.o
-
-sysentinel_metrics.o: $(wildcard /usr/src/kernels/7.1.8-200.fc44.x86_64/tools/objtool/objtool)
